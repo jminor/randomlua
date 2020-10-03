dcd5cf9c60bd80d1a697a8bf9412ee8dce4659eb
Joshua Minor <github@pixelverse.org>
2020-10-02 23:55:49 -0700

    Experimenting with demo seed.

--- demo.lua
+++ demo.lua
@@ -1,10 +1,12 @@
 require('randomlua')
 
-l1 = lcg(0) -- Linear congruential generator (Ansi C params)
-l2 = lcg(0, 'nr') --Linear congruential generator (Numerical recipes params)
-c1 = mwc(0) -- Multiply-with-carry (Ansi C params)
-c2 = mwc(0, 'nr') -- Multiply-with-carry (Numerical recipes params)
-m = twister(0) -- Mersenne twister
+seed = nil
+-- seed = 0
+l1 = lcg(seed) -- Linear congruential generator (Ansi C params)
+l2 = lcg(seed, 'nr') --Linear congruential generator (Numerical recipes params)
+c1 = mwc(seed) -- Multiply-with-carry (Ansi C params)
+c2 = mwc(seed, 'nr') -- Multiply-with-carry (Numerical recipes params)
+m = twister(seed) -- Mersenne twister
 
 for n = 1, 10 do
     io.write(string.format("%8u%8u%8u%8u%16u\n", l1:random(0), l2:random(0), c1:random(0), c2:random(0), m:random(0)))
